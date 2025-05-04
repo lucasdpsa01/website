b75648ef32c620f12b0b71412cb8f254cde35610
lucasdpsa01 <lucasdsa2801@gmail.com>
2025-05-04 13:19:19 -0300

    Aula 2

--- index.php
+++ index.php
@@ -1,23 +1,48 @@
+
 <?php
+    /* 
+    ========= Aula 1 ==========
+    
     echo"I love pizza <br>";
     echo"It really good";
     // this is a comment
-    /* this
+     this
         is
         a
         multiline
         comment
     */
+
+    /*
+    ========= Aula 2 ============
+    $name = "Paiva de Sá";
+    $food = "pizza";
+    $email = "fake@email.com";
+
+    $age = 20;
+    $users = 2;
+    $quantity = 3;
+
+    $gpa = 2.5;
+    $price = 4.99;
+    $tax_rate = 5.1;
+
+    $employed =  true;
+    $online = false;
+    $for_sale = true;
+
+    echo "Hello {$name}<br>";
+    echo "You like {$food}<br>";
+    echo "Your email is {$email}<br>";
+
+    echo "You are {$age} years old<br>";
+    echo "There are {$users} users online<br>";
+    echo "You would like to buy {$quantity} items<br>";
+    
+    echo "Your gba is: {$gpa}<br>";
+    echo "Your pizza is \${$price}<br>";
+    echo "The sales tax rates is: {$tax_rate}<br>";
+
+    echo "Online status: {$online}<br>";
+    */
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Oxi</title>
-</head>
-<body>
-    <br>
-    <button>order pizza</button>
-</body>
-</html>
