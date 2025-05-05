d0f1cba50427fe19dad33904663440c8aa45bfac
lucasdpsa01 <lucasdsa2801@gmail.com>
2025-05-04 23:25:03 -0300

    apaguei tudo

--- index.php
+++ index.php
@@ -1,69 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 2</title>
+</head>
+
+<body>
+    <form action="index.php" method="post">
+        <label>username: </label><br>
+        <input type="text" name="username"><br>
+        <label>password</label><br>
+        <input type="password" name="password"><br>
+        <input type="submit" value="Log in">
+    </form>
+</body>
+
+</html>
 
 <?php
-    /* 
-    ========= Aula 1 ==========
-    
-    echo"I love pizza <br>";
-    echo"It really good";
-    // this is a comment
-     this
-        is
-        a
-        multiline
-        comment
-    */
-
-    /*
-    ========= Aula 2 ============
-    $name = "Paiva de Sá";
-    $food = "pizza";
-    $email = "fake@email.com";
-
-    $age = 20;
-    $users = 2;
-    $quantity = 3;
-
-    $gpa = 2.5;
-    $price = 4.99;
-    $tax_rate = 5.1;
-
-    $employed =  true;
-    $online = false;
-    $for_sale = true;
-
-    echo "Hello {$name}<br>";
-    echo "You like {$food}<br>";
-    echo "Your email is {$email}<br>";
-
-    echo "You are {$age} years old<br>";
-    echo "There are {$users} users online<br>";
-    echo "You would like to buy {$quantity} items<br>";
-    
-    echo "Your gba is: {$gpa}<br>";
-    echo "Your pizza is \${$price}<br>";
-    echo "The sales tax rates is: {$tax_rate}<br>";
-
-    echo "Online status: {$online}<br>";
-    */
-    /* 
-    ============ Aula 3 =============
-    $x = 10;
-    $y = 2;
-    $z = null;
-    
-    //$z = $x + $y;
-    //$z = $x - $y;
-    //$z = $x * $y;
-    //$z = $x / $y;
-    //$z = $x ** $y;
-    $z = $x % $y;
-    echo $z;
-
-    $counter = 0;
-    $counter++;
-    $counter--;
-    $counter+=2;
-    $counter-=3;
-    echo $counter;
-    */
+    echo $_POST["username"] . "<br>";
+    echo "{$_POST["password"]} <br>";
 ?>
