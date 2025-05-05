7dfc2c8405af27cb2997a1dc5ff3b3cd51fece14
lucasdpsa01 <lucasdsa2801@gmail.com>
2025-05-04 23:00:38 -0300

    aula 3

--- index.php
+++ index.php
@@ -45,4 +45,18 @@
 
     echo "Online status: {$online}<br>";
     */
+    /* 
+    ============ Aula 3 =============
+    $x = 10;
+    $y = 2;
+    $z = null;
+    
+    //$z = $x + $y;
+    //$z = $x - $y;
+    //$z = $x * $y;
+    //$z = $x / $y;
+    //$z = $x ** $y;
+    $z = $x % $y;
+    echo $z;
+    */
 ?>
